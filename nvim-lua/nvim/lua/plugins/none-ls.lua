cedba6ddd533efd9e6a49740d44e97800dc78e97
ABetancourt03 <neutralzgamer@gmail.com>
2024-03-09 16:57:04 -0400

    Updating config

--- nvim-lua/nvim/lua/plugins/none-ls.lua
+++ nvim-lua/nvim/lua/plugins/none-ls.lua
@@ -1,15 +1,15 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-			},
-		})
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+      },
+    })
 
-		vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, {})
+  end,
 }
