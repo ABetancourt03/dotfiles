652ad6dff386db791fd89a8c7a3878186b4a8931
ABetancourt03 <neutralzgamer@gmail.com>
2024-03-15 09:01:10 -0400

    Update config

--- nvim/lua/plugins/gruvbox.lua
+++ nvim/lua/plugins/gruvbox.lua
@@ -1,6 +1,6 @@
 return {
-  "morhetz/gruvbox",
-  config = function()
-    vim.cmd.colorscheme("gruvbox")
-  end,
+	"morhetz/gruvbox",
+	--config = function()
+	--vim.cmd.colorscheme("gruvbox")
+	--end,
 }
--- nvim/lua/plugins/status-line.lua
+++ nvim/lua/plugins/status-line.lua
@@ -1,12 +1,12 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  config = function()
-    require('lualine').setup({
-      options = {
-        theme = 'gruvbox_dark',
-        section_separators = '',
-        component_separators = '|'
-      }
-    })
-  end
+	"nvim-lualine/lualine.nvim",
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "tokyonight",
+				section_separators = "",
+				component_separators = "|",
+			},
+		})
+	end,
 }
