849c666687a30f1c091695459bba27a008104be7
ABetancourt03 <neutralzgamer@gmail.com>
2024-03-15 20:40:03 -0400

    Update Config

--- nvim/lua/plugins/indentation.lua
+++ nvim/lua/plugins/indentation.lua
@@ -1,7 +1,7 @@
 return {
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		opts = {},
-	},
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = {}
+  },
 }
