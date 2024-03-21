ddd63a2edcb9ab60441700a0127a3fe41aeae6c2
Angelo Betancourt <angelo@MacBook-Pro-de-Angelo.local>
2024-03-20 22:02:12 -0400

    Updating Config

--- nvim/lua/plugins/status-line.lua
+++ nvim/lua/plugins/status-line.lua
@@ -1,12 +1,12 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "tokyonight",
-				section_separators = "",
-				component_separators = "|",
-			},
-		})
-	end,
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "auto",
+        section_separators = "",
+        component_separators = "|",
+      },
+    })
+  end,
 }
