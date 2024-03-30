b2d6d174e858760ca290ff22aa09156988797d46
ABetancourt03 <neutralzgamer@gmail.com>
2024-03-29 22:10:12 -0400

    Update Config

--- nvim/lua/core/plugin_config/telescope.lua
+++ nvim/lua/core/plugin_config/telescope.lua
@@ -1,7 +1,6 @@
 require("telescope").setup({ file_ignore_patterns = { "node%_modules/.*" } })
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", "<c-p>", builtin.find_files, {})
-vim.keymap.set("n", "<Space><Space>", builtin.oldfiles, {})
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<Space>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<Space>fh", builtin.help_tags, {})
