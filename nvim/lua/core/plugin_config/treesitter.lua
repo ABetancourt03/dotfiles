23271d5c3a729b61994a527babf74688bc3d6f76
Angelo <angelo@MacBook-Pro-de-Angelo.local>
2024-03-29 22:15:46 -0400

    Updating Config

--- nvim/lua/core/plugin_config/treesitter.lua
+++ nvim/lua/core/plugin_config/treesitter.lua
@@ -2,14 +2,19 @@ require("nvim-treesitter.configs").setup({
 	ensure_installed = {
 		"lua",
 		"vim",
+		"vimdoc",
 		"html",
 		"astro",
 		"javascript",
 		"typescript",
 		"python",
 		"sql",
+		"css",
+		"tsx",
+		"svelte",
+		"yaml",
+		"json",
 	},
-
 	sync_install = false,
 	auto_install = true,
 	highlight = {
