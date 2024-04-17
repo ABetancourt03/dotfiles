8ec9d45e2131d27963d8189d1cb3fe9cffea4e06
ABetancourt03 <neutralzgamer@gmail.com>
2024-04-17 12:33:18 -0400

    Update Config

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -4,19 +4,27 @@ vim.cmd("syntax enable")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.opt.clipboard = "unnamedplus"
+
 vim.opt.backspace = "2"
 vim.opt.showcmd = true
 vim.opt.laststatus = 2
 vim.opt.autowrite = true
 vim.opt.cursorline = true
 vim.opt.autoread = true
+vim.opt.showmode = false
+vim.opt.scrolloff = 10
+
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
 
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.shiftround = true
 vim.opt.expandtab = true
 
-vim.opt.clipboard = "unnamedplus"
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 
 vim.cmd([[ set noswapfile ]])
 vim.cmd([[ set termguicolors ]])
@@ -25,3 +33,11 @@ vim.wo.number = true
 vim.cmd("set relativenumber")
 
 vim.cmd("set matchpairs+=<:>")
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
