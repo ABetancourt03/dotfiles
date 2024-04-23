b4706528e7199e553de71daba85fef8a3095367a
ABetancourt03 <neutralzgamer@gmail.com>
2024-04-23 10:22:36 -0400

    Update wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,9 +4,14 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Tokyo Night"
 
+config.enable_scroll_bar = true
+
 config.default_prog = { "ubuntu" }
 
 config.font = wezterm.font("JetBrainsMono Nerd Font")
+
 config.font_size = 16
+
+config.window_background_opacity = 0.95
 
 return config
