c4b5c8d43c36538cdc575da1650e565e67e872dd
ABetancourt03 <neutralzgamer@gmail.com>
2024-04-27 17:10:18 -0400

    Move nvim config to a single config repository

--- nvim/lua/core/plugin_config/telescope.lua
+++
@@ -1,6 +0,0 @@
-require("telescope").setup({ file_ignore_patterns = { "node%_modules/.*" } })
-local builtin = require("telescope.builtin")
-
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<Space>fg", builtin.live_grep, {})
