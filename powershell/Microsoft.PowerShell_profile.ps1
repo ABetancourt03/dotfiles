287763cf9507d669ce1fb7fa5282839ff5adb79e
ABetancourt03 <angelob2207@gmail.com>
2025-05-17 17:40:39 -0400

    Update powershell profile

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,6 @@
 Invoke-Expression (&starship init powershell)
 Import-Module -Name Terminal-Icons
+Import-Module -Name PSReadLine
 
 function openDevDir { Set-Location ~/Dev && c }
 function openDotfiles { Set-Location ~/dotfiles && c }
@@ -15,10 +16,15 @@ function openIntellijIdea ($location) {
   C:\Program` Files\JetBrains\IntelliJ` IDEA` Community` Edition` 2024.3.4.1\bin\idea64.exe $location
 }
 
+function ezaLs { eza --icons=always }
+function ezaL { eza --icons=always -la }
+function ezaLl { eza --icons=always -ll }
+
 # ----- Aliases -----
 
-Set-Alias l ls
-Set-Alias ll ls
+Set-Alias ls ezaLs
+Set-Alias l ezaL
+Set-Alias ll ezaLl
 Set-Alias c clear
 Set-Alias v nvim
 Set-Alias vim nvim
@@ -37,8 +43,3 @@ Set-Alias ggpull gitPull
 Set-Alias ggpush gitPush
 
 fnm env --use-on-cd --shell powershell | Out-String | Invoke-Expression
-
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
