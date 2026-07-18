49c74c33efe8df09b9d758870fee7367457bf985
ABetancourt03 <angelob2207@gmail.com>
2026-07-18 00:00:03 -0400

    update nvim plugin config

---
+++ nvim/.config/nvim/lua/btw/plugins/snacks.lua
@@ -0,0 +1,76 @@
+return {
+	"folke/snacks.nvim",
+	dependencies = {
+		"echasnovski/mini.icons",
+	},
+	priority = 1000,
+	lazy = false,
+	opts = {
+		bigfile = { enabled = true },
+		dashboard = {
+			enabled = true,
+			preset = {
+				header = [[
+                                                                     
+       ████ ██████           █████      ██                     
+      ███████████             █████                             
+      █████████ ███████████████████ ███   ███████████   
+     █████████  ███    █████████████ █████ ██████████████   
+    █████████ ██████████ █████████ █████ █████ ████ █████   
+  ███████████ ███    ███ █████████ █████ █████ ████ █████  
+ ██████  █████████████████████ ████ █████ █████ ████ ██████ 
+        ]],
+			},
+		},
+		indent = { enabled = true },
+		input = { enabled = true },
+		git = { enabled = true },
+		picker = {
+			enabled = true,
+			sources = {
+				explorer = {
+					hidden = true,
+					ignored = true,
+					exclude = {
+						"**/.git/*",
+					},
+				},
+			},
+		},
+		notifier = { enabled = true },
+		quickfile = { enabled = true },
+		scroll = { enabled = false },
+		statuscolumn = { enabled = true },
+		words = { enabled = true },
+	},
+	keys = {
+		{
+			"<leader>lg",
+			function()
+				Snacks.lazygit()
+			end,
+			desc = "Lazygit",
+		},
+		{
+			"<leader><leader>",
+			function()
+				Snacks.picker.recent()
+			end,
+			desc = "Recent Files",
+		},
+		{
+			"<leader>fb",
+			function()
+				Snacks.picker.buffers()
+			end,
+			desc = "Buffers",
+		},
+		{
+			"<leader>fg",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Grep Files",
+		},
+	},
+}
