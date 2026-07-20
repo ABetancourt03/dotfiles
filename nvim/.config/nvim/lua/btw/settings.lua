c6c7b3744eba0d7d552a2b747a04e7f4a1302031
ABetancourt03 <angelob2207@gmail.com>
2026-07-19 22:07:09 -0400

    update nvim config

--- nvim/.config/nvim/lua/btw/settings.lua
+++ nvim/.config/nvim/lua/btw/settings.lua
@@ -18,7 +18,7 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = ""
+vim.opt.colorcolumn = "120"
 
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
